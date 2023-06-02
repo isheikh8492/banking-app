51c289968c54f9f66342a26383460f1fc0825e20
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2023-06-02 03:58:58 -0500

    project init

---
+++ src/test/java/com/capstone/bankingapp/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
